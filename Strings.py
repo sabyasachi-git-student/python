5adb9a63dd55ce8bcc3f92e799626f0f436f5909
Sabyasachi Gupta <sabyasachi.pmi@gmail.com>
2023-12-27 21:23:05 +0530

    starting with strings in python

---
+++ Strings.py
@@ -0,0 +1,33 @@
+#print("hello")
+#a = """Lorem ipsum dolor sit amet,
+#consectetur adipiscing elit,
+#sed do eiusmod tempor incididunt
+#ut labore et dolore magna aliqua."""
+#print(a)
+#a = '''Lorem ipsum dolor sit amet,
+#consectetur adipiscing elit,
+#sed do eiusmod tempor incididunt
+#ut labore et dolore magna aliqua.'''
+#print(a)
+#b="Hello World"
+#print(b[4])
+'''for x in "banana":
+ print(x)
+a = "Hello World"
+print(len(a))'''
+'''a= "hello world"
+print (a);'''
+'''a= "The best things in life are not free"
+if "free" in a:
+    print("yes, 'free' is present in string variable a.")
+    print(10>9)
+print(10==9)
+print(10<9)'''
+
+a=100
+b=33
+if b>a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+          
